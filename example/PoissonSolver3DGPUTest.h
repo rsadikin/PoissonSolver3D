9ea14d5e77f43dc43acee448d77180080fa6d3d7
rifki sadikin <rifki.sadikin@gmail.com>
2018-09-28 15:10:38 +0700

    First Commit

---
+++ example/PoissonSolver3DGPUTest.h
@@ -0,0 +1,8 @@
+#ifndef POISSONSOLVERGPU3DGPUTEST_H
+#define POISSONSOLVERGPU3DGPUTEST_H
+
+#include <iostream>
+#include "gpulib/PoissonSolver3DGPU.h"
+
+
+#endif
---
+++ gpuapp/PoissonSolver3DGPUTest.cpp
@@ -0,0 +1,6 @@
+#include "PoissonSolver3DGPUTest.h"
+
+int main()
+{
+	return 1;
+}
